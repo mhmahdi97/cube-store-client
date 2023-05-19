99e23a1c2fa56b837a7f06e89ca05813eacadc79
mhmahdi97 <mhmahdi97@gmail.com>
2023-05-19 22:34:55 +0600

    install and configured firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBK70i3UeegfDllXiAjuqZTHqTFSbHmFao",
+  authDomain: "cube-store-mhmahdi97.firebaseapp.com",
+  projectId: "cube-store-mhmahdi97",
+  storageBucket: "cube-store-mhmahdi97.appspot.com",
+  messagingSenderId: "13940802972",
+  appId: "1:13940802972:web:b8c4530110d3fb0565f459"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
