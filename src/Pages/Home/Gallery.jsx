1303ecba1d4be534f1cfb07f2ad061a1c403893d
mhmahdi97 <mhmahdi97@gmail.com>
2023-05-23 21:45:41 +0600

    implemented aos animation effects on hompage images

--- src/Pages/Home/Gallery.jsx
+++ src/Pages/Home/Gallery.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import cube1 from '../../assets/gallery/cube-1.png';
 import cube2 from '../../assets/gallery/cube-2.png';
 import cube3 from '../../assets/gallery/cube-3.png';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Gallery = () => {
+
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
+
     return (
         <section className="text-gray-600 body-font mt-14">
             <h1 className='text-5xl text-center text-black font-bold'>See Our Best Selling Cubes Gallery</h1>
             <div className="container px-5 py-14 mx-auto flex flex-wrap">
                 <div className="lg:w-2/3 mx-auto">
-                <div className="flex flex-wrap w-full bg-gray-100 py-32 px-10 relative mb-4">
+                <div data-aos="fade-right" className="flex flex-wrap w-full bg-gray-100 py-32 px-10 relative mb-4">
                     <img alt="gallery" className="w-full object-cover h-full object-center block opacity-50 absolute inset-0" src={cube1} />
                     <div className="text-center relative z-10 w-full">
                     <h2 className="text-2xl text-gray-900 font-medium title-font mb-2">CubingClassroom RS3 M 3x3 2020 - Stickerless</h2>
@@ -22,7 +30,7 @@ const Gallery = () => {
                 </div>
                 <div className="flex flex-wrap -mx-2">
                     <div className="px-2 w-1/2">
-                    <div className="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
+                    <div data-aos="fade-right" className="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
                         <img alt="gallery" className="w-full object-cover h-full object-center block opacity-50 absolute inset-0" src={cube2} />
                         <div className="text-center relative z-10 w-full">
                         <h2 className="text-xl text-gray-900 font-medium title-font mb-2">YuXin Little Magic 3x3x3 Cube - Stickerless 2021</h2>
@@ -35,7 +43,7 @@ const Gallery = () => {
                     </div>
                     </div>
                     <div className="px-2 w-1/2">
-                    <div className="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
+                    <div data-aos="fade-left" className="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
                         <img alt="gallery" className="w-full object-cover h-full object-center block opacity-50 absolute inset-0" src={cube3} />
                         <div className="text-center relative z-10 w-full">
                         <h2 className="text-xl text-gray-900 font-medium title-font mb-2">Meilong M 3x3</h2>
